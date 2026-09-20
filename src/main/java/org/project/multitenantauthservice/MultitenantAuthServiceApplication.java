2e570ea4106218bb17e5d189f0d224a3174fa69c
Salsabila <intern.salsabila@gli.id>
2026-09-20 17:36:06 +0700

    first commit

---
+++ src/main/java/org/project/multitenantauthservice/MultitenantAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package org.project.multitenantauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenantAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultitenantAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/multitenantauthservice/MultitenantAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.multitenantauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
