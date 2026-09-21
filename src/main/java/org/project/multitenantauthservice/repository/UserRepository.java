9b556aad1672af86aee0c75abd4c61ead3ded583
Salsabila <intern.salsabila@gli.id>
2026-09-21 19:09:09 +0700

    feat: create jwtauthFilter

--- src/main/java/org/project/multitenantauthservice/repository/UserRepository.java
+++ src/main/java/org/project/multitenantauthservice/repository/UserRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailOrUsername(String email, String username);
+
+    User findByUsername(String username);
 }
